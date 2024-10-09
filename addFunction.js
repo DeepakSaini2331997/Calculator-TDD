25d7506cece7f15bf639c4c482d46014028e7dd2
Deepak <deepaksaini2331997@gmail.com>
2024-10-09 18:58:17 +0530

    change is not reflected

--- addFunction.js
+++ addFunction.js
@@ -4,7 +4,7 @@ const add = (numbers)=>{
     if (!numbers.includes(",")) return parseInt(numbers);
 
     const nums = numbers.split(',')
-    //console.log(nums," my nummmmm"); return false;
+
     return nums.reduce((sum,num)=> sum + parseInt(num),0)
 
     
