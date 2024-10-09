b1ca9087b1747f7e944333998679452d29676bb0
Deepak <deepaksaini2331997@gmail.com>
2024-10-09 19:58:42 +0530

    handle negative number

--- addFunction.js
+++ addFunction.js
@@ -15,10 +15,22 @@ const add = (numbers)=>{
   
     // Split numbers and trim any whitespace
     const nums = numbers.split(delimiter).map(num => parseInt(num.trim()));
+
+    // check negative number
+    const negatives = nums.filter(num => num < 0);
+
+    if (negatives.length > 0) {
+        throw errorMessage(negatives);
+    }
+
     //console.log(nums,'my nums+++++++++'); return false;
     return nums.reduce((sum,num)=> sum + num,0)
 
     
+}
+
+const errorMessage = (negatives) => {
+    return new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
 }
 
 module.exports = {add}
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -26,4 +26,8 @@ describe('Calculator',()=>{
         expect(add("//;\n1;2")).toBe(3);
     })
 
+    test('should throw error if negative number', () => {
+        expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+    })
+
 })
